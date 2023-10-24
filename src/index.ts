6f3e5938d15e081832cfabb07206ca630ed0b42a
greensky-gs <david.hesliere@gmail.com>
2023-10-24 22:17:57 +0200

    feat: :rocket: Add all changes in index file

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+// Structures
+export { EmeraldApp } from './structures/EmeraldApp'
+export { EmeraldError } from './structures/EmeraldError'
+export { EmeraldUser } from './structures/EmeraldUser'
+
+// Managers
+export { EmeraldUsersManager } from './managers/EmeraldUsersManager'
+
+// Utils
+export { removeKey, resolvePerm, sqllise } from './utils/toolbox'
+
+// Types
+export {
+    pathLike,
+    appOptions,
+    PermLevel,
+    userConnectionAllowed,
+    emeraldUser,
+    updateUser,
+    userManagersOptions,
+    hashAlgorithm,
+    permComparation,
+    permComparator,
+    permComparationMethods,
+    loginCallback,
+    loginReason
+} from './typings/classes'
+export {
+    Colors,
+    permResolvable,
+    DefaultQueryResult,
+    QueryResult,
+    queryFunction,
+    Tables,
+    If,
+    routeLike
+} from './typings/core'
